a59d836c124c00563a8259dc904f974ca8cde2bb
Stevespear426 <stephen.k.spear@gmail.com>
2013-03-28 18:55:02 +0100

    l900: Remove devices asserts for now
    Change-Id: Id7bffb29db13814f8e32797df9b13bc0bc6984af
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,7 @@
 
 -include device/samsung/t0ltecdma/BoardCommonConfig.mk
 
-# assert
-TARGET_OTA_ASSERT_DEVICE := t0ltespr,l900,SPH-L900
+TARGET_OTA_ASSERT_SKIP := true
 
 # inherit from the proprietary version
 -include vendor/samsung/t0ltecdma/BoardConfigVendor.mk
