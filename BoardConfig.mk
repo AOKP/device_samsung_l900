e979d2884907c46294691e03e2bdf805f2d8efe8
sbrissen <sbrissen@hotmail.com>
2013-06-05 21:24:50 -0400

    l900: use t0lte instead of t0ltecdma
    Change-Id: If46e0ba83babd079bac2a6baf7f675a54454f8e0
    
    Conflicts:
    
    	cm.dependencies
    
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,8 +16,12 @@
 
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
+-include device/samsung/smdk4412-common/BoardCommonConfig.mk
+-include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
+-include device/samsung/t0lte/BoardCommonConfig.mk
 
--include device/samsung/t0ltecdma/BoardCommonConfig.mk
+# GPS
+BOARD_GPS_SET_PRIVACY := true
 
 TARGET_OTA_ASSERT_SKIP := true
 
